4a92577ba433b60d19dfba72d0d78a94a6abdd5d
anadantasp <caroldantasp@gmail.com>
2024-03-20 18:31:28 -0300

    criando o projeto

---
+++ src/main/java/br/com/fiap/livroteca/LivrotecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.livroteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/livroteca/LivrotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.livroteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
