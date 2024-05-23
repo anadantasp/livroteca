52019002a4d8cb6a813b927c95c4a59a3abcb7a6
anadantasp <caroldantasp@gmail.com>
2024-05-23 16:00:42 -0300

    validação

--- src/main/java/br/com/fiap/livroteca/livro/Livro.java
+++ src/main/java/br/com/fiap/livroteca/livro/Livro.java
@@ -2,10 +2,13 @@ package br.com.fiap.livroteca.livro;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +22,10 @@ public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @NotBlank(message = "Título obrigatório")
     private String titulo;
+    @Column(columnDefinition = "TEXT")
+    @Size(min = 10)
     private String descricao;
     private boolean disponibilidade;
     
--- src/main/java/br/com/fiap/livroteca/livro/LivroController.java
+++ src/main/java/br/com/fiap/livroteca/livro/LivroController.java
@@ -1,17 +1,20 @@
 package br.com.fiap.livroteca.livro;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("livros")
@@ -38,7 +41,7 @@ public class LivroController {
         return "livro/index";
     }
 
-    @GetMapping("/admin/delete/{id}")
+    @DeleteMapping("/admin/{id}")
     public String delete(@PathVariable UUID id, RedirectAttributes redirect){
         var livro = repository.findById(id);
 
@@ -54,12 +57,16 @@ public class LivroController {
     }
 
     @GetMapping("/admin/new")
-    public String form(){
+    public String form(Livro livro){
         return "livro/form";
     }
 
     @PostMapping("/admin")
-    public String create(Livro livro, RedirectAttributes redirect){
+    public String create(@Valid Livro livro, BindingResult result, RedirectAttributes redirect){
+        if(result.hasErrors()){
+            return "livro/form";
+        }
+
         livro.setDisponibilidade(true);
         repository.save(livro);
         redirect.addFlashAttribute("mensagem", "Livro cadastrado com sucesso");
